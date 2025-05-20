e170168983b6848233b5f22b7b313d8967f3d5b5
Ryan.Dias <ryanddsn0@gmail.com>
2025-05-20 20:46:35 -0300

    entrada e processamento do salario

--- dados_salarial.c
+++ dados_salarial.c
@@ -10,13 +10,21 @@ int validacao_inteiros(int entrada){
     return entrada;
 }
 
+float validacao_reais(float entrada){
+    while (scanf("%f", &entrada) != 1){
+        printf("INVALIDO, digite um valor real: ");
+        while(getchar() != '\n');
+    }
+    return entrada;
+}
+
 
 int main (int argc, char *argv[]){
     printf("-=- PREENCHA COM A AMOSTRA DA SUA REGIÃO -=-\n");
     printf("-=- Sera possivel informar IDADE, SEXO E SALARIO, ao processar os dados, entregamos a MEDIA SALARIAL, MAIOR E MENOR IDADE, QTD DE MULHERES QUE RECEBEM MAIS DE 2000,00\n");
     
     int entrada_idade, maior = 0, menor = 0, quantidade;
-    //float salario;
+    float montante, entrada_salario;
     //char sexo;
     
     printf("Quantidade de pessoas na amostra: ");
@@ -26,6 +34,11 @@ int main (int argc, char *argv[]){
         printf("Digite idade: ");
         int idade = validacao_inteiros(entrada_idade);
         
+        //Recebendo Salário e somando o salário a cada iteração
+        printf("Digite o seu salario: ");
+        float salario = validacao_reais(entrada_salario); 
+        montante += salario;
+
         //Verificanco se o dado atual digitado é maior ou menor que o anteior
         if (i == 0){
             menor = idade;
@@ -37,8 +50,12 @@ int main (int argc, char *argv[]){
                 maior = idade;
             }
         }
+
+
     }
 
+    float media =  montante/qtd_pessoas;
+    printf("Media: R$ %.2f", media);
     printf("\nMaior idade: %d\nMenor idade: %d\n", maior, menor);
     return 0;
 }
