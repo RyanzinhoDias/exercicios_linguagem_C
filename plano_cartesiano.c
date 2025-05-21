c6d6b81b8849fda96939660fe50658438d557ea2
Ryan.Dias <ryanddsn0@gmail.com>
2025-05-21 20:31:37 -0300

    programa finalizado

--- plano_cartesiano.c
+++ plano_cartesiano.c
@@ -18,7 +18,21 @@ int main (int argc, char argv[]){
         printf("Digite um valor para o eixo Y: ");
         eixo_y = verifica_inteiro(eixo_y);
 
+        if (eixo_x > 0 && eixo_y > 0){
+            printf(">>> PRIMEIRO QUADRANTE <<<\n");
+        } else if (eixo_x < 0 && eixo_y > 0){
+            printf(">>> SEGUNDO QUADRANTE <<<\n");
+        } else if (eixo_x < 0 && eixo_y < 0){
+            printf(">>> TERCEIRO QUADRANTE <<<\n");
+        } else if (eixo_x > 0 && eixo_y < 0) {
+            printf(">>> QUARTO QUADRANTE <<<\n");
+        } else {
+            printf("!!! COORDENADA NULA !!!\n");
+        }
+
     } while (!(eixo_x != 0 ^ eixo_y != 0));
     
+    printf("\n\n### PROGRAMA FINALIZADO ###\n");
+
     return 0;
 }
