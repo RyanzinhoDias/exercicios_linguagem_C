6f98a1b27ae75cf2411b11ce84dd323bb1c5f0ea
Ryan.Dias <ryanddsn0@gmail.com>
2025-06-04 13:41:16 -0300

    Iniciando programa

---
+++ estrutura_dados_homogeneas/exercicio01.c
@@ -0,0 +1,11 @@
+/* 1) Faça um programa que preencha um vetor de inteiros de tamanho 10 pedindo valores ao usuário.
+Em seguida, calcule e salve num segundo vetor o quadrado de cada elemento do primeiro vetor. Por
+fim, imprima os dois vetores. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    
+    return 0;
+}
