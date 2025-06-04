661e3e12db832001a413d4e522ca8c3da55dd504
Ryan.Dias <ryanddsn0@gmail.com>
2025-06-04 14:06:56 -0300

    recebendo dados do usuário

--- estrutura_dados_homogeneas/exercicio01.c
+++ estrutura_dados_homogeneas/exercicio01.c
@@ -5,7 +5,20 @@ fim, imprima os dois vetores. */
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
-    
+int main(int argc, char *argv[]) {
+    int entrada = 0;
+    int vetor_entrada[10];
+
+    for (int i = 1; i <= 10; i++) {
+        printf("Digite um valor para a %dª posição ", i);
+        scanf("%d", &entrada);
+        vetor_entrada[i] = entrada;
+    }
+
+    for (int p = 0; p <= 10; p++) {
+        printf(">>> Posição: %d\n", p);
+    }
+
     return 0;
+
 }
