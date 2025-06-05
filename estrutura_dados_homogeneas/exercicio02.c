867c0596c32fe42603c0f9d17af6f85b89669c67
Ryan.Dias <ryanddsn0@gmail.com>
2025-06-05 20:22:38 -0300

    Aqui já é possivel gerar números aleatório e armazenar no vetor

--- estrutura_dados_homogeneas/exercicio02.c
+++ estrutura_dados_homogeneas/exercicio02.c
@@ -5,6 +5,24 @@ em um terceiro vetor. Imprima os três vetores na tela.*/
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-    
+    int vetor1[25], vetor2[25];
+
+    for (int i = 0; i <= 25; i++){
+        vetor1[i] = rand() % 100;
+        vetor2[i] = rand() % 100;
+    }
+
+    for (int imprimir = 0; imprimir < 25; imprimir++){
+        printf("%d ", vetor1[imprimir]);
+    }
+
+    printf("\n");
+
+    for (int imprimir2 = 0; imprimir2 < 25; imprimir2++){
+        printf("%d ", vetor2[imprimir2]);
+    }
+
+    printf("\n");
+
     return 0;
 }
