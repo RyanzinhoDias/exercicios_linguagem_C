e75ec35df5c2434785ca0365404966ea631db736
Ryan.Dias <ryanddsn0@gmail.com>
2025-06-05 20:08:28 -0300

    criação do arquivo do exercicio02

---
+++ estrutura_dados_homogeneas/exercicio02.c
@@ -0,0 +1,10 @@
+/*2) Faça um programa que some o conteúdo de dois vetores de tamanho 25 e armazene o resultado
+em um terceiro vetor. Imprima os três vetores na tela.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    
+    return 0;
+}
