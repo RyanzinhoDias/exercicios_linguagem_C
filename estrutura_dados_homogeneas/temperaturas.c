3abd5022cbe4dfb39236c6e194029ca5c7897436
Ryan.Dias <ryanddsn0@gmail.com>
2025-06-25 20:10:01 -0300

    função de verificação de entrada criada

--- estrutura_dados_homogeneas/temperaturas.c
+++ estrutura_dados_homogeneas/temperaturas.c
@@ -4,17 +4,24 @@ mês por extenso: janeiro, fevereiro …). Desconsidere empates.*/
 
 #include <stdio.h> //biblioteca de input/output
 
+void verifica_float(float temperatura){
+    while (scanf("%f", &temperatura) != 1){
+        printf("[ERRO], digite uma temperatura valida, em graus Celsius: ");
+        while(getchar() != '\n');
+    }
+}
+
 int main(){
     float temperaturas[12]; //vetor para armazenar média de temperatura de cada mês (jan -> dez)
+    char meses[12][10] = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
 
     for (int i = 0; i < 12; i++){
         printf("Digite a media de temperatura do mês %d em graus Celsius: ", i+1);
-        //verificação se tipo de entrada é valida
-        while (scanf("%f", &temperaturas[i]) != 1){
-            printf("[ERRO], digite uma temperatura valida, em graus Celsius: ");
-            while(getchar() != '\n');
-        }
-        
+        verifica_float(temperaturas[i]);
+    }
+
+    for (int i = 0; i < 12; i++){
+        printf("%s\n", meses[i]);
     }
 
     return 0;
